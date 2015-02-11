0f6e5dbea76e55ccb3325464800e3757e708372c
Ian Andersen <ianderse@mac.com>
2015-02-11 16:11:42 -0700

    tests/implementation for hovering over top row

--- connect-four-test.js
+++ connect-four-test.js
@@ -1,6 +1,21 @@
 module('Setup')
 
 test('init adds the proper number of empty divs', function (assert) {
-  $(init)
+  $(init);
   assert.strictEqual($('#connect-four').find('.empty').length, 42, 'Divs are correctly placed')
 });
+
+module('Hovering')
+
+test('hovering over top-row class changes color', function (assert) {
+  $(init);
+  var hoveredItem = $('#connect-four').find('.top-row').mouseenter();
+  assert.ok((hoveredItem).hasClass('red'), 'Div changes class when hovered over')
+});
+
+test('leaving top-row div item changes color back to empty', function (assert) {
+  $(init);
+  var hoveredItem = $('#connect-four').find('.top-row').mouseenter();
+  hoveredItem.mouseleave();
+  assert.ok(!(hoveredItem).hasClass('red'), 'Div changes back to empty');
+})
--- connect-four.js
+++ connect-four.js
@@ -1,6 +1,8 @@
 var createBoard = function () {
   for(var i=0; i < 42; i++) {
-    if(i % 7 === 0) {
+    if(i <= 6) {
+      $("<div class='empty top-row'></div>").appendTo('#connect-four')
+    } else if(i % 7 === 0) {
       $("<br>").appendTo('#connect-four')
       $("<div class='empty'></div>").appendTo('#connect-four')
     } else {
@@ -10,6 +12,13 @@ var createBoard = function () {
   }
 }
 
+var bindHoverToTopRow = function () {
+  $('.top-row').hover(function () {
+    $(this).toggleClass('red');
+  });
+};
+
 var init = function () {
   createBoard();
+  bindHoverToTopRow();
 };
